2c644e61d9d8c04cc7355a75ac12835996f6cd32
calvincho95 <calvincho95@naver.com>
2024-07-24 18:40:31 +0900

    test daehwan

--- vite/src/pages/DaehwanPage.tsx
+++ vite/src/pages/DaehwanPage.tsx
@@ -1,14 +1,7 @@
 import { FC } from "react";
 
-
 const DaehwanPage: FC = () => {
-
-  return (
-        <div>
-            대환 체크 : 
-        </div>
-
-  );
+  return <div>대환 체크 : test</div>;
 };
 
 export default DaehwanPage;
