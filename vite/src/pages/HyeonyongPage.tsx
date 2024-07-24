67ff6514c6764c0b981822c45976dafeb3c2cccc
YangHyunYong <43340172+YangHyunYong@users.noreply.github.com>
2024-07-24 18:13:09 +0900

    test2

--- vite/src/pages/HyeonyongPage.tsx
+++ vite/src/pages/HyeonyongPage.tsx
@@ -1,14 +1,7 @@
 import { FC } from "react";
 
-
 const HyeonyongPage: FC = () => {
-
-  return (
-        <div>
-            현용 체크 : 
-        </div>
-
-  );
+  return <div>현용 체크 : 2222222</div>;
 };
 
 export default HyeonyongPage;
