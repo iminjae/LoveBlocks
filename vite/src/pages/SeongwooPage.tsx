205ca3f7e37b8ea1b99cfb1b5b1a04b1a77de12e
starlash7 <yang92sw@gmail.com>
2024-07-24 18:40:31 +0900

    test sungwoo

--- vite/src/pages/SeongwooPage.tsx
+++ vite/src/pages/SeongwooPage.tsx
@@ -1,14 +1,7 @@
 import { FC } from "react";
 
-
 const SeongwooPage: FC = () => {
-
-  return (
-        <div>
-            성우 체크 : 
-        </div>
-
-  );
+  return <div>성우 체크확인 :</div>;
 };
 
 export default SeongwooPage;
