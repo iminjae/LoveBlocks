81822bcbecaf54f94da9ffc44b9e7961afb6ad05
YangHyunYong <9935ok9938@gmail.com>
2024-08-06 16:07:12 +0900

    feat: metamask 로그인 추가

---
+++ vite/src/pages/HyunyongPage.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+import TokenCollect from "../components/TokenCollect";
+
+const HyunyongPage: FC = () => {
+  return <div>
+    토큰수집 해보자
+    <TokenCollect></TokenCollect>
+  </div>;
+};
+
+export default HyunyongPage;
