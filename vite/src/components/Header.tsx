6609ce5c3f28456a59edc64cd49553e822581349
iminjae <83637802+iminjae@users.noreply.github.com>
2024-08-21 11:31:58 +0900

    header 수정

--- vite/src/components/Header.tsx
+++ vite/src/components/Header.tsx
@@ -86,35 +86,39 @@ const Header: FC<HeaderProps> = ({ signer, setSigner, setProvider }) => {
   };
 
   return (
-    <div className="container-style h-20 flex justify-between items-center p-10 bg-blue-200">
-      <button onClick={() => navigate("/")}>Home</button>
 
-      <button onClick={() => navigate("/donation")}>기부하기</button>
+    <header className="fixed top-0 left-0 right-0 bg-white shadow-md z-50">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center py-4">
+          <button className="text-xl font-bold text-toss-dark" onClick={() => navigate("/")}>LOVEBLOCKS</button>
+          <nav className="space-x-8">
+            <button  className="text-toss-gray hover:text-toss-blue" onClick={() => navigate("/donation")}>기부하기</button>
+            <button  className="text-toss-gray hover:text-toss-blue" onClick={() => navigate("/applyDonatePJ")}>기부 프로젝트 신청</button>
+            <button  className="text-toss-gray hover:text-toss-blue" onClick={() => navigate("/signup")}>현용</button>
+            <button  className="text-toss-gray hover:text-toss-blue" onClick={() => navigate("/sungwoo")}>성우</button>
+            <button  className="text-toss-gray hover:text-toss-blue" onClick={() => navigate("/daehwan")}>대환</button>
+            <button  className="text-toss-gray hover:text-toss-blue" onClick={() => navigate("/minjae")}>민재</button>
+            {localStorage.getItem("addr") ? (
+              <button className="text-toss-gray hover:text-toss-blue" onClick={onClickLogout}>
+                {localStorage.getItem("addr")?.substring(0, 5)}...
+                {localStorage.getItem("addr")?.substring(15, 20)}
+              </button>
+            ) : signer ? (
+              <button className="text-toss-gray hover:text-toss-blue" onClick={onClickLogout}>
+                {signer.address.substring(0, 5)}...
+                {signer.address.substring(15, 20)}
+              </button>
+            ) : (
+              <button onClick={onClickMetamask}>Connect Wallet</button>
+            )}
 
-      <button onClick={() => navigate("/applyDonatePJ")}>기부 프로젝트 신청</button>
 
-      <button onClick={() => navigate("/signup")}>현용</button>
+          </nav>
+        </div>
+      </header>
 
-      <button onClick={() => navigate("/sungwoo")}>성우</button>
 
-      <button onClick={() => navigate("/daehwan")}>대환</button>
 
-      <button onClick={() => navigate("/minjae")}>민재</button>
-
-      {localStorage.getItem("addr") ? (
-        <button onClick={onClickLogout}>
-          {localStorage.getItem("addr")?.substring(0, 5)}...
-          {localStorage.getItem("addr")?.substring(15, 20)}
-        </button>
-      ) : signer ? (
-        <button onClick={onClickLogout}>
-          {signer.address.substring(0, 5)}...
-          {signer.address.substring(15, 20)}
-        </button>
-      ) : (
-        <button onClick={onClickMetamask}>Connect Wallet</button>
-      )}
-    </div>
+   
   );
 };
 
