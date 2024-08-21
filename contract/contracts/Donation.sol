8098aa48be76eaf295fc0df0d7dae9c991d11099
YangHyunYong <43340172+YangHyunYong@users.noreply.github.com>
2024-08-22 02:41:12 +0900

    update: signUp/applyPJ page, donation/swap Contract

--- contract/contracts/Donation.sol
+++ contract/contracts/Donation.sol
@@ -2,36 +2,40 @@
 pragma solidity >=0.8.2 <0.9.0;
 
 import "@openzeppelin/contracts/utils/Strings.sol";
+import "./DonationSwap.sol";
 
-interface IERC20Permit {/* 서명 permit interface*/
+interface IERC20Permit {
+    /* 서명 permit interface*/
 
     function permit(
-        address owner,      //토큰소유자
-        address spender,    //승인할계정
-        uint256 value,      //토큰량
-        uint256 deadline,   //서명유효기간
-        uint8 v,            
+        address owner, //토큰소유자
+        address spender, //승인할계정
+        uint256 value, //토큰량
+        uint256 deadline, //서명유효기간
+        uint8 v,
         bytes32 r,
         bytes32 s
     ) external;
 
-    function nonces(address owner) external view returns (uint256);//서명자 논스값(중복방지)
+    function nonces(address owner) external view returns (uint256); //서명자 논스값(중복방지)
 }
 
-interface IERC20 {
-    function transferFrom(address from, address to, uint256 amount) external returns (bool);
-    function balanceOf(address account) external view returns (uint256);
-}
+// interface IERC20 {
+//     function transferFrom(
+//         address from,
+//         address to,
+//         uint256 amount
+//     ) external returns (bool);
+//     function balanceOf(address account) external view returns (uint256);
+// }
 
-contract Donation {
-
+contract Donation is TokenSwap {
     /* 기부 서명 받을때 데이터 */
     struct SignatureData {
-        address owner;      //토큰소유자
-        address token;      //토큰주소
-        uint256 amount;     //토큰량
-        uint256 deadline;   //서명유효기간
-
+        address owner; //토큰소유자
+        address token; //토큰주소
+        uint256 amount; //토큰량
+        uint256 deadline; //서명유효기간
         /* v,r,s 는 서명값임 */
         uint8 v;
         bytes32 r;
@@ -46,11 +50,17 @@ contract Donation {
     }
 
     /* 기부단체가 기부프로젝트 신청시 정보 저장(기부단체 => pinata에 저장된 CID)*/
-    mapping(address => string) applyDonationPJInfoList;
+    // mapping(address => string) applyDonationPJInfoList;
+    struct Organization {
+        address orgAddr;
+        string cid;
+    }
+
+    Organization[] projectList;
+    Organization selectedCharity;
 
     //서명 받은거 permit처리 (approve)
     function permit(SignatureData calldata signature) external {
-       
         IERC20Permit tokenPermit = IERC20Permit(signature.token);
 
         tokenPermit.permit(
@@ -65,11 +75,9 @@ contract Donation {
     }
 
     function transferFrom(DBSignatureData[] memory signature) external {
-
         require(signature.length > 0, "no signature data.");
 
-        for(uint i = 0; i < signature.length; i++){
-
+        for (uint i = 0; i < signature.length; i++) {
             IERC20 token = IERC20(signature[i].tokenAddress);
 
             require(
@@ -84,24 +92,70 @@ contract Donation {
     }
 
     //받은 토큰 조회
-    function getContractTokenBalance(address token)public view returns (uint256) {
+    function getContractTokenBalance(
+        address token
+    ) public view returns (uint256) {
         return IERC20(token).balanceOf(address(this));
     }
 
-     // 소유자의 논스 값을 조회
-    function getNonces(address token, address owner) public view returns (uint256) {
+    // 소유자의 논스 값을 조회
+    function getNonces(
+        address token,
+        address owner
+    ) public view returns (uint256) {
         return IERC20Permit(token).nonces(owner);
     }
 
     //기부단체 신청정보 저장
-    function setApplyDonationPJInfo(address organization, string memory cid) public {
-
-        applyDonationPJInfoList[organization] = cid;
+    function setApplyDonationPJInfo(
+        address organization,
+        string memory cid
+    ) public {
+        // applyDonationPJInfoList[organization] = cid;
+        for (uint i = 0; i < projectList.length; i++) {
+            if (projectList[i].orgAddr == organization)
+                revert("Already set information");
+        }
+        projectList.push(Organization({orgAddr: organization, cid: cid}));
     }
 
     //기부단체 신청정보 조회 (화면에서 CID로 처리)
-    function getApplyDonationPJInfo(address organization) public view returns(string memory){ 
+    function getApplyDonationPJInfo(
+        address organization
+    ) public view returns (string memory) {
+        for (uint i = 0; i < projectList.length; i++) {
+            if (projectList[i].orgAddr == organization) {
+                return projectList[i].cid;
+            }
+        }
+        return "";
+    }
 
-        return applyDonationPJInfoList[organization];
+    function getProjectList() public view returns (Organization[] memory) {
+        return projectList;
+    }
+
+    function removeProject() public {
+        delete selectedCharity;
+        delete projectList;
+    }
+
+    function setSelectOrganization(Organization memory organization) public {
+        selectedCharity = organization;
+    }
+
+    function selectRandomCharity() public {
+        require(projectList.length > 0, "No charities registered");
+
+        uint256 randomIndex = uint256(
+            keccak256(
+                abi.encodePacked(block.timestamp, block.difficulty, msg.sender)
+            )
+        ) % projectList.length;
+        selectedCharity = projectList[randomIndex];
+    }
+
+    function getSelectedCharity() public view returns (Organization memory) {
+        return selectedCharity;
     }
 }
