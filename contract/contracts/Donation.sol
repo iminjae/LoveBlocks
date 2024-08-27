97df04def0bfe9839b00868e498e4a7c0e1c0c3c
YangHyunYong <43340172+YangHyunYong@users.noreply.github.com>
2024-08-28 04:12:53 +0900

    temp merge

--- contract/contracts/Donation.sol
+++ contract/contracts/Donation.sol
@@ -56,7 +56,7 @@ contract Donation is TokenSwap {
         string cid;
     }
 
-    struct CollectToken{
+    struct CollectToken {
         address tokenAddr;
         uint amount;
     }
@@ -65,6 +65,8 @@ contract Donation is TokenSwap {
     Organization[] projectList;
     Organization selectedCharity;
     mapping(address => string[]) receipts;
+    uint public totalDonation;
+    uint public usedDonation;
 
     //서명 받은거 permit처리 (approve)
     function permit(SignatureData calldata signature) external {
@@ -87,19 +89,24 @@ contract Donation is TokenSwap {
         for (uint i = 0; i < signature.length; i++) {
             IERC20 token = IERC20(signature[i].tokenAddress);
 
-            bool flag=false;
+            bool flag = false;
 
-            for(uint j=0;j<collectTokens.length;j++){
-                if(collectTokens[j].tokenAddr == signature[i].tokenAddress){
+            for (uint j = 0; j < collectTokens.length; j++) {
+                if (collectTokens[j].tokenAddr == signature[i].tokenAddress) {
                     collectTokens[j].amount += signature[i].amount;
-                    flag=true;
+                    flag = true;
                     break;
                 }
             }
-            if(!flag){
-                collectTokens.push(CollectToken({tokenAddr:signature[i].tokenAddress,amount:signature[i].amount}));
+            if (!flag) {
+                collectTokens.push(
+                    CollectToken({
+                        tokenAddr: signature[i].tokenAddress,
+                        amount: signature[i].amount
+                    })
+                );
             }
-            
+
             require(
                 token.transferFrom(
                     signature[i].owner,
@@ -111,13 +118,17 @@ contract Donation is TokenSwap {
         }
     }
 
-    function getCollectToken() public view returns(CollectToken[] memory){
+    function getCollectToken() public view returns (CollectToken[] memory) {
         return collectTokens;
     }
 
     function swapUSDT() public {
-        for(uint i=0;i<collectTokens.length;i++){
-            swapExactTokensForUSDT(collectTokens[i].tokenAddr, collectTokens[i].amount, 0x0293E2Ebd6a497E4b0825F3cef8057d78fE8e69e);
+        for (uint i = 0; i < collectTokens.length; i++) {
+            swapExactTokensForUSDT(
+                collectTokens[i].tokenAddr,
+                collectTokens[i].amount,
+                0x0293E2Ebd6a497E4b0825F3cef8057d78fE8e69e
+            );
         }
     }
 
@@ -189,7 +200,7 @@ contract Donation is TokenSwap {
         return selectedCharity;
     }
 
-    function setReceipts(string[] memory _receipts) public {
+    function setReceipts(string[] memory _receipts, uint _totalPrice) public {
         uint len = receipts[msg.sender].length;
         uint inputLen = _receipts.length;
         string[] memory temp = new string[](len + inputLen);
@@ -201,9 +212,10 @@ contract Donation is TokenSwap {
         }
         delete receipts[msg.sender];
         receipts[msg.sender] = temp;
+        usedDonation += _totalPrice;
     }
 
-    function getReceipts() public view returns (string[] memory) {
-        return receipts[msg.sender];
+    function getReceipts(address _addr) public view returns (string[] memory) {
+        return receipts[_addr];
     }
 }
